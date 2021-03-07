de81b93a596462fe1b327df4c7767edecb3d349d
vsalmeron87 <nitos0629@gmail.com>
2021-03-07 11:30:44 -0500

    I am a developer right now this is my first commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,11 @@
+package com.collection;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i <5 ; i++){
+            System.out.println("Hello batch 8");
+            Thread.sleep(2000);
+        }
+    }
+}
