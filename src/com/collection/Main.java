b2eb56c51c10d40507a8881d4af1179350de36ac
vsalmeron87 <nitos0629@gmail.com>
2021-03-07 12:48:29 -0500

    3rd Commit attempt

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i <6 ; i++){
+        for (int i = 0; i <7 ; i++){
             System.out.println("Hello batch 8");
             Thread.sleep(2000);
         }
